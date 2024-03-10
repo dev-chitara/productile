7c730e73565edc8f66253cb8e96912c398c457c7
Dev Chitara <devchitara2003@gmail.com>
2024-03-10 17:18:09 +0530

    Some changes

--- migrations.py
+++ migrations.py
@@ -1,4 +1,4 @@
 from db_setup import engine, Base
-from models import products,categories, brands, product_images, base
+from models import products, categories, brands, product_images, base
 
 Base.metadata.create_all(bind=engine)
